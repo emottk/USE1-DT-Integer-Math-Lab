e0081ab7470ded145f1663eaeeff70ed43ce9a0e
emottk <emily.kowal@postlight.com>
2016-10-03 16:03:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Integer and Float Math Practice
+
+Follow the instructions in the comments in `lab.rb`.		
+Then `cd` into this directory and run `Ruby lab.rb` in your terminal to check your answers!
---
+++ lab.rb
@@ -0,0 +1,36 @@
+#Replace each zero with the answer to the math problem.
+#When you run the program, each statement should evaluate to true
+
+#For example
+puts "Time to Do Some Math!"
+
+puts 5*7 == 35
+
+puts 8-4 == 0
+
+puts 3 * (7-5) == 0
+
+puts 4**2 == 0
+
+puts 3 - (10/10) == 0
+
+puts 5.0 * 0.5 == 0
+
+puts (9+4)/2 == 0
+
+puts ((4-2)**2)+2 == 0
+
+#Create your own math problems to equal each number.
+#Use each operation at least once.
+#Replace the zero with your math problem.
+puts "Time to Create Your Own!"
+
+puts 0 == 90
+
+puts 0 == 37
+
+puts 0 == 81
+
+puts 0 == 1
+
+puts 0 == 23.0
